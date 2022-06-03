0260073f4010f525b22ae88d4e18ecb8a30939bb
Matthew Songer <matt91893@gmail.com>
2022-06-03 12:17:44 -0500

    change file structure for routes and add to server

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//Express middleware
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Default response for any other request (not Found)
+
+app.use('/api', apiRoutes);
+
 app.use((req, res) => {
     res.status(404).end();
 });
